8e10988b3e75b3baaaa2e06948c5578f5884c87f
Giulia Buttros <105403316+GiuliaButtros@users.noreply.github.com>
2023-06-07 21:08:02 -0300

    script - funções drag & drop/copy OK

--- JavaScript/galaxia.js
+++ JavaScript/galaxia.js
@@ -2,39 +2,66 @@ const openModalButton = document.querySelector("#open-modal");
 const closeModalButton = document.querySelector("#close-modal");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fade");
-
-const celestialBody = document.querySelectorAll(".modal-body button");
-
 const toggleModal = () => {
-    modal.classList.toggle("hide"); // toggle: A classe está, ela removida / a classe não está, ela é adicionada
-    fade.classList.toggle("hide");
-}
+    modal.classList.toggle("hide"); // toggle: A classe está, ela é removida / a classe não está, ela é adicionada
+    /*fade.classList.toggle("hide");*/
 
-/*
-resumo do array:
-const toggleModal = () => {
+    /*
+    resumo do array:
+    const toggleModal = () => {
     [modal,fade].forEach((el) => el.classList.toggle("hide"));  
 
+    }
+    */
 }
-*/
+
+const bodies = document.querySelector('.bodies').cloneNode(true); 
+// assim só seleciona o primeiro filho, mas quando usa querySelectorAll, aparece erro nas funções dragStart e dragEnd
+
+const display = document.querySelector('.display');
 
 //evento padrão em vários elementos:
 
-
-[openModalButton, closeModalButton, fade].forEach((el) => {
+[openModalButton, closeModalButton /*, fade*/].forEach((el) => {
     el.addEventListener("click", () => toggleModal());
 })
 
+//Drag & Drop 
 
-for (let index = 0; index < celestialBody.length; index++) {
-    celestialBody[index].onclick = ({ target }) => {
-        const body = target.getAttribute("id");
+bodies.addEventListener('dragstart', dragStart);
+bodies.addEventListener('dragend', dragEnd);
 
-        console.log(body);
-    };
+display.addEventListener('dragover', dragOver); // MUDAR FADE PARA DISPLAY
+display.addEventListener('dragenter', dragEnter);
+display.addEventListener('dragleave', dragLeave);
+display.addEventListener('drop', dragDrop);
+
+// Drag & Drop functions
+
+function dragStart(){
+    console.log('start'); // quando gragStart a div fade some??
+}
+
+function dragEnd() {
+    this.className = 'bodies';
     
 }
 
-function updateDisplay(body){
+function dragOver(e) {
+    e.preventDefault();
+    
+}
 
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered'    
+}
+
+function dragLeave() {
+    this.className = 'display'
+}
+
+function dragDrop() {
+    this.className = 'display'
+    this.append(bodies);
 }
